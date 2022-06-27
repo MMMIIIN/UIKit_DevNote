63057fad0ee89b9d1aa215d884675cb89434ba42
chemi <wbsldj7645@naver.com>
2022-06-27 22:40:24 +0900

    [ADD] segmentcotrol

--- UIKit_DevNote/Screens/Letter/LetterViewController.swift
+++ UIKit_DevNote/Screens/Letter/LetterViewController.swift
@@ -11,6 +11,17 @@ import SnapKit
 
 class LetterViewController: BaseViewController {
 
+    private enum LetterState: String, CaseIterable {
+        case sent = "보낸 쪽지"
+        case received = "받은 쪽지"
+    }
+
+    var segmentIndex: Int = 0 {
+        didSet {
+            letterSegmentCotrol.selectedSegmentIndex = segmentIndex
+        }
+    }
+
     // MARK: - property
 
     private let letterLabel: UILabel = {
@@ -21,11 +32,30 @@ class LetterViewController: BaseViewController {
         return label
     }()
 
+    private lazy var letterSegmentCotrol: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["받은 쪽지", "보낸 쪽지"])
+        let font = UIFont.font(.regular, ofSize: 14)
+        let normalTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, .font: font]
+        let selectedTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black, .font: font]
+        control.selectedSegmentIndex = segmentIndex
+        control.setTitleTextAttributes(normalTextAttributes, for: .normal)
+        control.setTitleTextAttributes(selectedTextAttributes, for: .selected)
+        control.selectedSegmentTintColor = .white
+        control.backgroundColor = .darkGrey004
+        control.addTarget(self, action: #selector(changedIndexValue(_:)), for: .valueChanged)
+        return control
+    }()
+
+    @objc
+    private func changedIndexValue(_ sender: UISegmentedControl) {
+        segmentIndex = sender.selectedSegmentIndex
+    }
+
+
     // MARK: - life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -35,6 +65,13 @@ class LetterViewController: BaseViewController {
         letterLabel.snp.makeConstraints {
             $0.leading.equalTo(20)
             $0.top.equalTo(66)
+        }
+
+        view.addSubview(letterSegmentCotrol)
+        letterSegmentCotrol.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(126)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(44)
         }
     }
 
