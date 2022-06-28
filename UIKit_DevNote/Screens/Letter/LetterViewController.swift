341b57bdfe55b27c293e4c8a00b14a477285b8f7
chemi <wbsldj7645@naver.com>
2022-06-28 21:57:02 +0900

    [CHORE] collectionview 왜 안뜨는지 모르겠음

--- UIKit_DevNote/Screens/Letter/LetterViewController.swift
+++ UIKit_DevNote/Screens/Letter/LetterViewController.swift
@@ -11,6 +11,8 @@ import SnapKit
 
 class LetterViewController: BaseViewController {
 
+    let data: [String] = ["abc", "asdf", "abc", "asdf", "abc", "asdf", "abc", "asdf"]
+
     var segmentIndex: Int = 0 {
         didSet {
             letterSegmentCotrol.selectedSegmentIndex = segmentIndex
@@ -26,7 +28,6 @@ class LetterViewController: BaseViewController {
         label.font = .font(.regular, ofSize: 34)
         return label
     }()
-
     private lazy var letterSegmentCotrol: UISegmentedControl = {
         let control = UISegmentedControl(items: ["받은 쪽지", "보낸 쪽지"])
         let font = UIFont.font(.regular, ofSize: 14)
@@ -40,12 +41,23 @@ class LetterViewController: BaseViewController {
         control.addTarget(self, action: #selector(changedIndexValue(_:)), for: .valueChanged)
         return control
     }()
-
-    @objc
-    private func changedIndexValue(_ sender: UISegmentedControl) {
-        segmentIndex = sender.selectedSegmentIndex
-    }
-
+    private let letterLayout: UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.sectionInset = UIEdgeInsets(top: 18, left: 16, bottom: 18, right: 16)
+        flowLayout.minimumLineSpacing = 33
+        flowLayout.sectionHeadersPinToVisibleBounds = true
+        flowLayout.estimatedItemSize = CGSize(width: UIScreen.main.bounds.size.width - 32, height: 48)
+        return flowLayout
+    }()
+    private lazy var letterCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: letterLayout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        collectionView.register(LetterCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
 
     // MARK: - life cycle
 
@@ -58,8 +70,8 @@ class LetterViewController: BaseViewController {
     override func render() {
         view.addSubview(letterLabel)
         letterLabel.snp.makeConstraints {
-            $0.leading.equalTo(20)
-            $0.top.equalTo(66)
+            $0.leading.equalToSuperview().inset(20)
+            $0.top.equalToSuperview().inset(66)
         }
 
         view.addSubview(letterSegmentCotrol)
@@ -68,6 +80,39 @@ class LetterViewController: BaseViewController {
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(44)
         }
+
+        view.addSubview(letterCollectionView)
+        letterCollectionView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.bottom.equalTo(letterSegmentCotrol.snp.bottom).inset(32)
+        }
     }
 
+    // MARK: - func
+
+    @objc
+    private func changedIndexValue(_ sender: UISegmentedControl) {
+        segmentIndex = sender.selectedSegmentIndex
+    }
+
+}
+
+extension LetterViewController: UICollectionViewDelegate { }
+
+extension LetterViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: LetterCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! LetterCollectionViewCell
+        cell.setLabelText(data[indexPath.row])
+        return cell
+    }
+}
+
+extension LetterViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 500)
+    }
 }
