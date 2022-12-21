16d01bf9df4fd258994528e3db60c839bdf22df8
chemi <wbsldj7645@naver.com>
2022-12-21 22:58:48 +0900

    [CHORE] FlexLayout import문제 해결

--- UIKit_DevNote/Screens/Pin&FlexLayout/PinAndFlexLayoutViewController.swift
+++ UIKit_DevNote/Screens/Pin&FlexLayout/PinAndFlexLayoutViewController.swift
@@ -5,4 +5,31 @@
 //  Created by Mingwan Choi on 2022/12/20.
 //
 
-import Foundation
+import UIKit
+
+import FlexLayout
+import PinLayout
+
+final class PinAndFlexViewController: BaseViewController {
+    
+    // MARK: - property
+    
+    private let flexView = UIView()
+    private let examView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        return view
+    }()
+    
+    // MARK: - life cycle
+    
+    override func render() {
+        view.addSubview(flexView)
+        flexView.flex.define { flex in
+            flex.addItem(examView)
+        }
+        
+        view.addSubview(examView)
+        examView.pin.all(10)
+    }
+}
