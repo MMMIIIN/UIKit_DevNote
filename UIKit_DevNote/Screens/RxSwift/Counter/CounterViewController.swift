df3cec47715e0e18c2fa5dded263d546aaf12c3c
chemi <wbsldj7645@naver.com>
2023-01-02 15:30:10 +0900

    [CHORE] RxSwift View들 분리

---
+++ UIKit_DevNote/Screens/RxSwift/Counter/CounterViewController.swift
@@ -0,0 +1,87 @@
+//
+//  CounterViewController.swift
+//  UIKit_DevNote
+//
+//  Created by Mingwan Choi on 2023/01/02.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class CounterViewController: BaseViewController {
+    private var disposeBag = DisposeBag()
+    private let countRelay = BehaviorRelay<Int>(value: 10)
+    
+    private let countLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.font = .systemFont(ofSize: 24)
+        return label
+    }()
+    private let minusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("-", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.blue, for: .highlighted)
+        return button
+    }()
+    private let addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("+", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.blue, for: .highlighted)
+        return button
+    }()
+    
+    // MARK: - life cycle
+    
+    override func configUI() {
+        super.configUI()
+        bind()
+    }
+    
+    override func render() {
+        view.addSubview(countLabel)
+        countLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(100)
+        }
+        
+        view.addSubview(minusButton)
+        minusButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(40)
+            $0.bottom.equalToSuperview().inset(100)
+            $0.width.equalTo(100)
+        }
+        
+        view.addSubview(addButton)
+        addButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(40)
+            $0.bottom.equalToSuperview().inset(100)
+            $0.width.equalTo(100)
+        }
+    }
+    
+    private func bind() {
+        minusButton.rx.tap
+            .map { -1 }
+            .bind(to: countRelay)
+            .disposed(by: disposeBag)
+        
+        addButton.rx.tap
+            .map { 1 }
+            .bind(to: countRelay)
+            .disposed(by: disposeBag)
+        
+        countRelay
+            .scan(0, accumulator: { $0 + $1 })
+            .withUnretained(self)
+            .map { "\($1)"}
+            .bind(to: countLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
+
--- UIKit_DevNote/Screens/RxSwift/RxSwiftViewController.swift
+++ UIKit_DevNote/Screens/RxSwift/RxSwiftViewController.swift
@@ -12,75 +12,36 @@ import RxSwift
 import SnapKit
 
 final class RxSwiftViewController: BaseViewController {
-    private var disposeBag = DisposeBag()
-    private let countRelay = BehaviorRelay<Int>(value: 10)
-    
-    private let countLabel: UILabel = {
-        let label = UILabel()
-        label.text = "0"
-        label.font = .systemFont(ofSize: 24)
-        return label
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        return view
     }()
-    private let minusButton: UIButton = {
+    private lazy var counterButton: UIButton = {
         let button = UIButton()
-        button.setTitle("-", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
-        button.setTitleColor(.blue, for: .highlighted)
+        button.setTitle("Counter", for: .normal)
+        button.backgroundColor = .systemPurple
+        let action = UIAction { [weak self] _ in
+            self?.pushCounterViewController()
+        }
+        button.addAction(action, for: .touchUpInside)
         return button
     }()
-    private let addButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("+", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
-        button.setTitleColor(.blue, for: .highlighted)
-        return button
-    }()
-    
-    // MARK: - life cycle
-    
-    override func configUI() {
-        super.configUI()
-        bind()
-    }
     
     override func render() {
-        view.addSubview(countLabel)
-        countLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(100)
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
         
-        view.addSubview(minusButton)
-        minusButton.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(40)
-            $0.bottom.equalToSuperview().inset(100)
-            $0.width.equalTo(100)
+        stackView.addSubview(counterButton)
+        counterButton.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(60)
         }
         
-        view.addSubview(addButton)
-        addButton.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(40)
-            $0.bottom.equalToSuperview().inset(100)
-            $0.width.equalTo(100)
-        }
     }
     
-    private func bind() {
-        minusButton.rx.tap
-            .map { -1 }
-            .bind(to: countRelay)
-            .disposed(by: disposeBag)
-        
-        addButton.rx.tap
-            .map { 1 }
-            .bind(to: countRelay)
-            .disposed(by: disposeBag)
-        
-        countRelay
-            .scan(0, accumulator: { $0 + $1 })
-            .withUnretained(self)
-            .map { "\($1)"}
-            .bind(to: countLabel.rx.text)
-            .disposed(by: disposeBag)
+    private func pushCounterViewController() {
+        self.navigationController?.pushViewController(CounterViewController(), animated: true)
     }
 }
